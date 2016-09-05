8a46bcb48eca33aaaad6f4a9b99591dc172e753f
TemaSir <tianxin@gsoft.cc>
2016-09-05 09:42:43 +0800

    Initial Commit

---
+++ customImageCropping/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  customImageCropping
+//
+//  Created by tianXin on 16/9/5.
+//  Copyright © 2016年 Tema. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
